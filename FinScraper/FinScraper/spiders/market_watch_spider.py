ea5e5dfa4fcc76fd5e018231268a1eef8585d4a1
angelocioffi <angelo.cioffi@utexas.edu>
2019-03-06 10:25:26 -0600

    Fixed User_Agent issue, but scraper still fails to follow links

--- FinScraper/FinScraper/spiders/market_watch_spider.py
+++ FinScraper/FinScraper/spiders/market_watch_spider.py
@@ -8,6 +8,11 @@ class MarketWatchSpider(scrapy.Spider):
         'https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101',
     ]
 
+    def start_requests(self):
+        headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+        for url in self.start_urls:
+            yield scrapy.Request(url, headers=headers)
+
     def parse(self, response):
         #follow links to articles
         for href in response.css('searchResult + a::attr(href)'):
@@ -18,5 +23,5 @@ class MarketWatchSpider(scrapy.Spider):
             return response.css(query).get(default='').strip()
 
         yield {
-            'title': extract_with_css('h1#article-headline::text')
+            'title': extract_with_css('h1#article-headline::text').get()
         }
