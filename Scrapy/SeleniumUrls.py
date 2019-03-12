215f9b987820a77b7554698ee538580571595348
angelocioffi <angelo.cioffi@utexas.edu>
2019-03-12 13:07:21 -0500

    Expanded dataset

--- Scrapy/SeleniumUrls.py
+++ Scrapy/SeleniumUrls.py
@@ -4,8 +4,8 @@ import json
 
 searchURLs = ["https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
  			  "https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=201",
- 			  "https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=301"]
+ 			  "https://www.marketwatch.com/search?q=amazon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=amazon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101"]
 
 
 driver = webdriver.Firefox()
@@ -13,28 +13,27 @@ data = {}
 
 for searchURL in searchURLs:
 	try:
-		driver.get(searchURLs[0])
+		driver.get(searchURL)
 		articleURLs = driver.find_elements_by_class_name("searchresult")
 		urls = list()
 
 		for x in articleURLs:
-			urls.append(articleURLs[1].find_element_by_css_selector('a').get_attribute('href'))
-			file.write(url + "\n")
+			urls.append(x.find_element_by_css_selector('a').get_attribute('href'))
 
-			for url in urls:
-				if "marketwatch.com" in urls[0]:
-					driver.get(urls[0])
-					if "marketwatch.com" in driver.current_url:
-						articleBody = driver.find_elements_by_xpath('//div[@id="article-body"]/p')
-						articleString = ""
-						for sentence in articleBody:
-							sentenceText = sentence.text
-							articleString += sentenceText
+		for url in urls:
+			if "marketwatch.com" in url:
+				driver.get(url)
+				if "marketwatch.com" in driver.current_url:
+					articleBody = driver.find_elements_by_xpath('//div[@id="article-body"]/p')
+					articleString = ""
+					for sentence in articleBody:
+						sentenceText = sentence.text
+						articleString += sentenceText
 
-						data[urls[0]] = {
-							'title': driver.find_element_by_css_selector('h1#article-headline').text,
-							'article': articleString
-						}
+					data[url] = {
+						'title': driver.find_element_by_css_selector('h1#article-headline').text,
+						'article': articleString
+					}
 	except:
 		pass
 	
