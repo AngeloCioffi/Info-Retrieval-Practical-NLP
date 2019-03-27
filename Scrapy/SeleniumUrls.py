2b2a8f30420fffb12ce9d7054381ff9a1713dc01
angelocioffi <angelo.cioffi@utexas.edu>
2019-03-27 10:09:18 -0500

    Expanded data collection.

--- Scrapy/SeleniumUrls.py
+++ Scrapy/SeleniumUrls.py
@@ -2,13 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import json
 
+# searching largest public companies
+# https://www.forbes.com/top-public-companies/list/
+
 searchURLs = ["https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
  			  "https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
  			  "https://www.marketwatch.com/search?q=amazon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=amazon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101"]
+ 			  "https://www.marketwatch.com/search?q=amazon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=apple&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=apple&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=berkshire+hathaway&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=berkshire+hathaway&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=jpmorgan+chase&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=jpmorgan+chase&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=exxonmobile&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=exxonmobil&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=at%26t&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=at%26t&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=bank+of+america&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=bank+of+america&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=wells+fargo&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=wells+fargo&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=verizon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=verizon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=microsoft&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=microsoft&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=wal-mart&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=wal-mart&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=alphabet&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=alphabet&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
+ 			  "https://www.marketwatch.com/search?q=unitedhealth+group&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+ 			  "https://www.marketwatch.com/search?q=unitedhealth+group&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101"]
 
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 data = {}
 
 for searchURL in searchURLs:
