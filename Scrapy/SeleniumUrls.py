348cc3c56e6e5409e26ba982afddf6348ea608e4
angelocioffi <angelo.cioffi@utexas.edu>
2019-03-29 12:59:37 -0500

    Updated scraper, scraped data file, and debugged the spacy parsing script.

--- Scrapy/SeleniumUrls.py
+++ Scrapy/SeleniumUrls.py
@@ -1,69 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import json
+import urllib
 
 # searching largest public companies
 # https://www.forbes.com/top-public-companies/list/
 
-searchURLs = ["https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=facebook&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=amazon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=amazon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=apple&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=apple&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=berkshire+hathaway&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=berkshire+hathaway&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=jpmorgan+chase&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=jpmorgan+chase&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=exxonmobile&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=exxonmobil&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=at%26t&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=at%26t&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=bank+of+america&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=bank+of+america&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=wells+fargo&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=wells+fargo&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=verizon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=verizon&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=microsoft&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=microsoft&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=wal-mart&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=wal-mart&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=alphabet&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=alphabet&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101",
- 			  "https://www.marketwatch.com/search?q=unitedhealth+group&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
- 			  "https://www.marketwatch.com/search?q=unitedhealth+group&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101"]
+companies = ["facebook", "amazon", "apple", "berkshire+hathaway", "jpmorgan+chase", "exxonmobile", "at%26t", 
+			 "bank+of+america", "wells+fargo", "verizon", "microsoft", "wal-mart", "alphabet", "unitedhealth+group"]
 
 
 driver = webdriver.Chrome()
-data = {}
+data = []
 
-for searchURL in searchURLs:
-	try:
-		driver.get(searchURL)
-		articleURLs = driver.find_elements_by_class_name("searchresult")
-		urls = list()
+for company in companies:
+	searchURLs = ["https://www.marketwatch.com/search?q=" + company + "&m=Keyword&rpp=100&mp=2005&bd=false&rs=true",
+				  "https://www.marketwatch.com/search?q=" + company + "&m=Keyword&rpp=100&mp=2005&bd=false&rs=true&o=101"]
 
-		for x in articleURLs:
-			urls.append(x.find_element_by_css_selector('a').get_attribute('href'))
+	company_stripped = urllib.unquote(company).replace("+", " ")
+	for searchURL in searchURLs:
+		try:
+			driver.get(searchURL)
+			articleURLs = driver.find_elements_by_class_name("searchresult")
+			urls = list()
 
-		for url in urls:
-			if "marketwatch.com" in url:
-				driver.get(url)
-				if "marketwatch.com" in driver.current_url:
-					articleBody = driver.find_elements_by_xpath('//div[@id="article-body"]/p')
-					articleString = ""
-					for sentence in articleBody:
-						sentenceText = sentence.text
-						articleString += sentenceText
+			for x in articleURLs:
+				urls.append(x.find_element_by_css_selector('a').get_attribute('href'))
 
-					data[url] = {
-						'title': driver.find_element_by_css_selector('h1#article-headline').text,
-						'article': articleString,
-						'date': driver.find_element_by_xpath('//p[@id="published-timestamp"]/span').text
-					}
-	except:
-		pass
+			for url in urls:
+				if "marketwatch.com" in url:
+					driver.get(url)
+					if "marketwatch.com" in driver.current_url:
+						articleBody = driver.find_elements_by_xpath('//div[@id="article-body"]/p')
+						articleString = ""
+						for sentence in articleBody:
+							sentenceText = sentence.text
+							articleString += sentenceText
+
+						data.append({
+							'title': driver.find_element_by_css_selector('h1#article-headline').text,
+							'article': articleString,
+							'date': driver.find_element_by_xpath('//p[@id="published-timestamp"]/span').text,
+							'company': company_stripped,
+							'url': url
+						})
+		except:
+			pass
 	
 
 with open('MarketWatchData.txt', 'w') as outfile:
--- test_spacy.py
+++ test_spacy.py
@@ -1,7 +1,7 @@
 import en_core_web_sm
 from spacy import displacy
 import re
-import spacy.symbols
+from spacy.symbols import nsubj, dobj, poss, amod, pobj, npadvmod, acomp, nmod, prep, VERB
 
 nlp = en_core_web_sm.load()
 doc_text = """When Warren Buffett\u2019s annual letter to Berkshire Hathaway shareholders was published last weekend, media coverage focused on the chairman and CEO\u2019s mea culpaabout Berkshire\u2019s investment in Kraft Heinz KHC, -0.55% his lament that he couldn\u2019t find the next big investment to buy, the accounting change that caused Berkshire to take a $20 billion write-down, and, once again, his failure to announce any formal succession plans even though Buffett is 88 years old and Vice Chairman Charlie Munger is 95.But one thing jumped out at me in his annual letter and a subsequent extended interview with Becky Quick of CNBC: his acknowledgment that his best stock pickers hadn\u2019t beaten the market and his tacit admission that investors couldn\u2019t expect Berkshire Hathaway BRK.A, +0.02% BRK.B, -0.06%  to do so in the future, either.Todd Combs and Ted Wechsler joined Berkshire as investment managers within a year of each other in 2011-2012. \u201cOverall, they are a tiny bit behind the S&P SPX, +0.08%   each by just almost the same margin over the same time,\u201d Buffett told Quick."""
@@ -17,44 +17,44 @@ for token in doc:
     if token.dep == nsubj and token.head.pos == VERB:
         verb = token.head
         triple = {}
-        has_propn = token.pos == PNOUN 
+        has_propn = token.pos_ == "PNOUN" 
 
         subject_expanded = ""
         for subject_child in token.children:
-        	subj_mods = [poss, amod, compound]
-        	if any(subject_child in sm for x in subj_mods):
+        	subj_mods = ["poss", "amod", "compound"]
+        	if any(x == subject_child.dep_ for x in subj_mods):
         		subject_expanded = subject_child.text + token.text
-        		if subject_child.pos == PNOUN:
+        		if subject_child.pos_ == "PNOUN":
         			has_propn = True
 
         right_dep = [dobj, pobj, npadvmod, acomp]
         object_expanded = ""
         for right_token in verb.children:
-            if any(right_token in dp for x in right_dep):
+            if any(x == right_token.dep for x in right_dep):
             	for child in right_token.children: #will this cause error with no children?
-            		if right_token == dobj:
-            			dobj_mods = [nummod, compound, nmod]
-            			if any(child in dm for x in dobj_mods):
+            		if right_token.dep == dobj:
+            			dobj_mods = ["nummod", "compound", "nmod"]
+            			if any(x == child.dep_ for x in dobj_mods):
             				object_expanded = child.text + right_token.text
-            				if subject_child.pos == PNOUN:
+            				if subject_child.pos_ == "PNOUN":
             					has_propn = True
-            		else if right_token == pobj:
-            			if child == nummod:
+            		elif right_token.dep == pobj:
+            			if child.dep_ == "nummod":
             				object_expanded = child.text + right_token.text
-            				if subject_child.pos == PNOUN:
+            				if subject_child.pos_ == "PNOUN":
             					has_propn = True
-            		else if right_token == acomp:
-            			if child == npadvmod:
+            		elif right_token.dep == acomp:
+            			if child.dep == npadvmod:
             				object_expanded = child.text + right_token.text
-            				if subject_child.pos == PNOUN:
+            				if subject_child.pos_ == "PNOUN":
             					has_propn = True
             	if has_propn:
             		triple = [verb.text, token.text, subject_expanded, right_token.text, object_expanded]
-            else if right_token == prep:
+            elif right_token.dep == prep:
             	for child in right_token.children:
-            		if child == pobj:
+            		if child.dep == pobj:
             			object_expanded = right_token.text + child.text 
-            			if child.pos == PNOUN:
+            			if child.pos_ == "PNOUN":
             				has_propn = True
             			if has_propn:
             				triple = [verb.text, nsubj.text, right_token.text, object_expanded]
